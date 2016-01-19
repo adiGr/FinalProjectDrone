33653b2ca52ea8119dcd34dec7c176eb0187839d
adiGr <adi8554@gmail.com>
2016-01-19 08:59:06 +0200

    Update HelloSkySensors.c

--- code/HelloSkySensors.c
+++ code/HelloSkySensors.c
@@ -27,17 +27,18 @@ int px4_simple_app_main(int argc, char *argv[])
 	px4_pollfd_struct_t fds[] = {
     { .fd = sensor_sub_fd,   .events = POLLIN },
 	};
-	while (true) {
-    int poll_ret = px4_poll(fds, 1, 1000);
+    while (true) {
+        int poll_ret = px4_poll(fds, 1, 1000);
 
-    if (fds[0].revents & POLLIN) {
-        struct sensor_combined_s raw;
-        /* copy sensors raw data into local buffer */
-        orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
-        printf("[px4_simple_app] Accelerometer:\t%8.4f\t%8.4f\t%8.4f\n",
+        if (fds[0].revents & POLLIN) {
+            struct sensor_combined_s raw;
+            /* copy sensors raw data into local buffer */
+            orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+            printf("[px4_simple_app] Accelerometer:\t%8.4f\t%8.4f\t%8.4f\n",
                     (double)raw.accelerometer_m_s2[0],
                     (double)raw.accelerometer_m_s2[1],
                     (double)raw.accelerometer_m_s2[2]);
+        }
     }
-	return 0;
+    return 0;
 }
