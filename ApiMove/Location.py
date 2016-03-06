5d571986792a33771b883399cbac1cf1b56d9dc9
adiGr <adi8554@gmail.com>
2016-03-06 16:09:48 +0200

    more function to location: equals, convert from vehicle
    get from json and print the location

--- ApiMove/Location.py
+++ ApiMove/Location.py
@@ -86,3 +86,39 @@ class Location:
             self.altitude = float (_altitude)
         except ValueError:
             self.altitude = 0
+
+	
+    #######################################
+    # Equals
+    #######################################
+    def equalsTo(self, cmpLocation):
+        if cmpLocation.latitude == self.latitude and cmpLocation.longitude == self.longitude and cmpLocation.altitude == self.altitude:
+            return True
+        else:
+            return False
+
+
+    #######################################
+    # Convert From Vehicle Location
+    #######################################
+    def setFromVehicleLocation(self, vecLoc):
+        self.latitude = vecLoc.lat
+        self.longitude = vecLoc.lon
+        self.altitude = vecLoc.alt
+
+
+    #######################################
+    # Convert From JSON Location
+    #######################################
+    def setFromJSONLocation(self, JSONLoc):
+        self.latitude = JSONLoc['latitude']
+        self.longitude = JSONLoc['longitude']
+        self.altitude = JSONLoc['altitude']
+
+
+    #######################################
+    # Print Location
+    #######################################
+    def displayLocation(self):
+        print "|  latitude: %0.2f  |  longitude: %0.2f  |  altitude: %0.2f  " % (self.latitude, self.longitude, self.altitude)
+
