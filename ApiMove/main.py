a81fbddedb15df2f27c1258f126f035685f3dfbc
adiGr <adi8554@gmail.com>
2016-03-22 14:22:23 +0200

    check the forward,backward right and left by simulation need to check triangle on the right method

--- ApiMove/main.py
+++ ApiMove/main.py
@@ -58,33 +58,31 @@ def main():
     vehicle.close()
     """
     drone = Drone('tcp:127.0.0.1:5760')
+    #drone = Drone('udp:127.0.0.1:14550')
     print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
     print "the heading is : %s" % drone.vehicle.heading
     drone.take_off(2)
     print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
-    drone.up(3)
-    print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
+    #drone.up(3)
+    #print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
     #drone.down(1)
     #print " global_relative_frame down drone: %s\n" %  drone.vehicle.location.global_relative_frame
     #drone.down(4)
     #print " global_relative_frame down drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    #drone.forward(2)
+    print "the heading is : %s" % drone.vehicle.heading
+
+    print " global_relative_frame down drone: %s\n" %  drone.vehicle.location.global_relative_frame
+    #drone.forward(5)  # work!!!!!!
+    #drone.backwards(5)  # work!!!!!!
+
     #print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    #drone.backwards(2)
-    #print " global_frame forward drone: %s\n" %  drone.vehicle.location.global_frame
+    #drone.forward(2)  #workkkkkk!!!!!!
+    #drone.backwards(2)  # work!!!!!!!!
+
+    print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
+    #drone.move_right(10)  # workkk !!!!!!
+    #drone.move_right(-10)  # workkk !!!!!!
     print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
-
-    point1 = LocationGlobalRelative(-30.546456, 156.165218, 20)
-
-    drone.vehicle.simple_goto(point1)
-    time.sleep(100)
-
-    print " global_relative_frame: %s\n" %  drone.vehicle.location.global_relative_frame
-
-    #print "the heading is : %s" % drone.vehicle.heading
-    #drone.turn_right(90)
-    #drone.move_right(5)
-    #print "the heading is : %s" % drone.vehicle.heading
 
 
     # Close vehicle object before exiting script
