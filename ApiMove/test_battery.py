f1ce5afc4f1bc9393e53677e623f3a5378dce566
adiGr <adi8554@gmail.com>
2016-05-05 12:25:53 +0300

    test for function set level in battery class

--- ApiMove/test_battery.py
+++ ApiMove/test_battery.py
@@ -12,10 +12,10 @@ CHAR_NUMBER = '48'
 
 
 class bet:
-    def __init__(self, lat, lon, alt):
-        self.lat = lat
-        self.lon = lon
-        self.alt = alt
+    def __init__(self, voltage, current, level):
+        self.voltage = voltage
+        self.current = current
+        self.level = level
 
 
 class TestBattery(TestCase):
@@ -37,10 +37,35 @@ class TestBattery(TestCase):
         # check ZERO
         battery_test.set_volt(0)
         self.assertEqual(battery_test.get_volt(), 0)
-        #check char that non number
+        # check char that non number
         battery_test.set_volt(CHARACTER)
         self.assertEqual(battery_test.get_volt(), 0)
         #check char that  number
         battery_test.set_volt(CHAR_NUMBER)
         self.assertEqual(battery_test.get_volt(), int(CHAR_NUMBER))
 
+
+    def test_set_level(self):
+        sitl = SITL()
+        sitl.download('copter', '3.3', verbose=True)
+        sitl_args = ['-I0', '--model', 'quad', '--home=-35.363261,149.165230,584,353']
+        sitl.launch(sitl_args, await_ready=True, restart=True)
+        print "Connecting to vehicle on: 'tcp:127.0.0.1:5760'"
+        vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
+        print " Battery: %s" % vehicle.battery
+        battery_test = Battery(vehicle.battery)
+        # check number negative the limit
+        battery_test.set_level(NEGATIVE_NUMBER)
+        self.assertEqual(battery_test.level, 0)
+        # check number positive the limit
+        battery_test.set_level(POSITIVE_NUMBER)
+        self.assertEqual(battery_test.get_level(), POSITIVE_NUMBER)
+        # check ZERO
+        battery_test.set_level(0)
+        self.assertEqual(battery_test.get_level(), 0)
+        #check char that non number
+        battery_test.set_level(CHARACTER)
+        self.assertEqual(battery_test.get_level(), 0)
+        #check char that  number
+        battery_test.set_level(CHAR_NUMBER)
+        self.assertEqual(battery_test.get_level(), int(CHAR_NUMBER))
