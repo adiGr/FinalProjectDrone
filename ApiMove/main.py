93ca353ae2a290ce70b973f3312ddd877ce45b0f
adiGr <adi8554@gmail.com>
2016-06-15 18:29:59 +0300

    change angle and distance

--- ApiMove/main.py
+++ ApiMove/main.py
@@ -9,35 +9,36 @@ from Battery import Battery
 from dronekit import *
 
 def main():
+    """
     print "Start simulator (SITL)"
     sitl = SITL()
     sitl.download('copter', '3.3', verbose=True)
-    sitl_args = ['-I0', '--model', 'quad', '--home=31.768797, 35.193556,0,315 ']
+    sitl_args = ['-I0', '--model', 'quad', '--home=31.768797, 35.193556,0,225 ']
     sitl.launch(sitl_args, await_ready=True, restart=True)
     # Import DroneKit-Python
     # Connect to the Vehicle.
+    """
 
-
-    drone = Drone('tcp:127.0.0.1:5760')
-    #drone = Drone("com3")
+    #drone = Drone('tcp:127.0.0.1:5760')
+    drone = Drone("com3")
     drone.take_off(3)
 
     #drone.up(3)
     print "#######################################################################################"
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    drone.move_forward(2)
+    drone.move_forward(3)
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
     print "#######################################################################################"
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    drone.move_right(2)
+    drone.move_right(3)
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
     print "#######################################################################################"
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    drone.move_backwards(2)
+    drone.move_backwards(3)
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
     print "#######################################################################################"
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
-    drone.move_left(2)
+    drone.move_left(3)
     print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
     print "#######################################################################################"
     #print " global_relative_frame forward drone: %s\n" %  drone.vehicle.location.global_relative_frame
@@ -52,7 +53,7 @@ def main():
     drone.vehicle.close()
     #f.close()
     # Shut down simulator
-    sitl.stop()
+    #sitl.stop()
     print("Completed")
 
 if __name__ == '__main__':
